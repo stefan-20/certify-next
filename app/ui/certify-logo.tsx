d231cfc94dab9b92531544750a11593b2f80ec83
stefan-20 <schleglstefan@gmail.com>
2023-12-24 11:35:32 +0100

    initial commit

---
+++ app/ui/certify-logo.tsx
@@ -0,0 +1,12 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function CertifyLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <p className="text-[44px]">Certify</p>
+    </div>
+  );
+}
